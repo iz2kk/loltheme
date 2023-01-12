73e696cd6205d18a56993c19b111893078f453dd
iz2kk <hqvblog@gmail.com>
2023-01-12 17:52:07 +0700

    z

--- PushGit.bat
+++ PushGit.bat
@@ -1,6 +1,6 @@
 @echo off
 set /p Br="Enter Branch Name: "
-set CDNLINK="https://cdn.jsdelivr.net/gh/iz2kk/loltheme@%Br%/IZI_Theme.css";
+set CDNLINK=https://cdn.jsdelivr.net/gh/iz2kk/loltheme@%Br%/IZI_Theme.css
 
 git add *
 git commit -m  z
